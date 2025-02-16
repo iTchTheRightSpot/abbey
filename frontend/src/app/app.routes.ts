3d76112ee3e2862d3446c1db8643af7d2e5248e9
iTchTheRightSpot <emmanueluluabuike@gmail.com>
2025-02-16 06:07:50 -0500

    initialized angular

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from '@angular/router';
+
+export const CoreRoutes = {
+  HOME: 'home',
+  AUTH: 'auth'
+};
+
+export const routes: Routes = [
+  {
+    path: CoreRoutes.HOME,
+    loadComponent: () =>
+      import('./home/home.component').then((m) => m.HomeComponent)
+  },
+  {
+    path: CoreRoutes.AUTH,
+    loadComponent: () =>
+      import('./auth/auth.component').then((m) => m.AuthComponent)
+  },
+  { path: '**', redirectTo: `/${CoreRoutes.HOME}` }
+];
